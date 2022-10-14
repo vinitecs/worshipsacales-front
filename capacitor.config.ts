e096d3656d23590ec86cd9d31fd03f8582282cf2
vinicius <viniciusamalia@gmail.com>
2022-10-13 22:41:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'worship_scales_web',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
